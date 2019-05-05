09da092904ebcd41a3fad8306a22b80ad957581e
Mirco <Mirco@DESKTOP-A55DTAV>
2019-05-05 19:47:35 +0200

    push

--- Guida C++ 18.2 Copy Assignment/src/main.cpp
+++ Guida C++ 18.2 Copy Assignment/src/main.cpp
@@ -7,6 +7,6 @@ int main() {
 	Point2D p1(10, 10); // chiama il costruttore Point2D(double, double)
 	Point2D p2(p1); // ok, chiama il costruttore di copia
 	Point2D p3(Point2D(10, 10)); // ok solo se il costruttore di copia accetta un valore temporaneo, cioè una const reference.
-	//system("pause");
+	system("pause");
 	return 0;
 }
